36106649ce96d077849340576782c95199090fe7
Mikkel Theut <mikkeltheut@gmail.com>
2024-10-07 12:59:00 +0200

    remove slashes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [  { path: 'login', loadComponent:  () => import ("./components/login/login.component") },
-    { path: '/register', loadComponent: () => import('./components/register/register.component') },
-    { path: '/vault', loadComponent: ()=> import('./components/vault/vault.component') },
+    { path: 'register', loadComponent: () => import('./components/register/register.component') },
+    { path: 'vault', loadComponent: ()=> import('./components/vault/vault.component') },
     { path: '**', redirectTo: 'login' }];
